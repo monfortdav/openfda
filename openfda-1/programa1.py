9fbfc6612fda8b73cde611c90eaf20b44dd83f0d
parallels <david_monfort_martin@hotmail.com>
2018-03-26 23:53:33 +0200

    Creamos el fichero programa1

--- openfda-1/programa1.py
+++ openfda-1/programa1.py
@@ -3,14 +3,12 @@ import json
 
 headers = {'User-Agent': 'http-client'}
 
-skip_number=0
-while True:
-    conn = http.client.HTTPSConnection("api.fda.gov")
-    conn.request("GET", "/drug/label.json", None, headers)
-    r1 = conn.getresponse()
-    print(r1.status, r1.reason)
-    estructura_json = r1.read().decode("utf-8")
-    conn.close()
+conn = http.client.HTTPSConnection("api.fda.gov")
+conn.request("GET", "/drug/label.json", None, headers)
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+estructura_json = r1.read().decode("utf-8")
+conn.close()
 
 #escribimos a fichero lo recibido
 #fichero = open ('label.json', 'w')
@@ -19,15 +17,11 @@ while True:
 #fin escribir
 
 
-    label = json.loads(estructura_json)
-    for i in range(len(label['results'])):
-        medicamento_info=label['results'][i]
-        print ('ID: ',medicamento_info['id'])
-        if (medicamento_info['openfda']):
-            print ('Fabricante: ', medicamento_info['openfda']['manufacturer_name'][0])
-    if (len(label['results'])<100):
-        break
-    skip_number=skip_number+100
+label = json.loads(estructura_json)
+medicamento_info=label['results'][0]
 
+print ('ID: ',medicamento_info['id'])
 print ('Proposito: ',medicamento_info['purpose'][0])
+
+print ('Fabricante: ',medicamento_info['openfda']['manufacturer_name'][0])
 
