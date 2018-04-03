341d6a790a875faf74051d470332c0696aa4dd16
parallels <david_monfort_martin@hotmail.com>
2018-04-03 12:09:09 +0200

    Creamos el fichero programa2

--- openfda-1/programa2.py
+++ openfda-1/programa2.py
@@ -10,15 +10,9 @@ print(r1.status, r1.reason)
 estructura_json = r1.read().decode("utf-8")
 conn.close()
 
-#escribimos a fichero lo recibido
-#fichero = open ('label.json', 'w')
-#fichero.write(estructura_json)
-#fichero.close()
-#fin escribir
-
 
 label = json.loads(estructura_json)
 for i in range (len (label['results'])):
-    medicamento_info=label['results'][i]
+    info_medicamento=label['results'][i]
 
-    print ('ID: ',medicamento_info['id'])
+    print ('ID: ',info_medicamento['id'])
