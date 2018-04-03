153520936264e6828aa7582956ac65eab56fb93d
parallels <david_monfort_martin@hotmail.com>
2018-04-03 12:10:18 +0200

    Creamos el fichero programa3

--- openfda-2/programa3.py
+++ openfda-2/programa3.py
@@ -6,21 +6,19 @@ headers = {'User-Agent': 'http-client'}
 skip_number=0
 while True:
     conn = http.client.HTTPSConnection("api.fda.gov")
-    #conn.request("GET", "/drug/label.json?limit=20&search=active_ingredient:%22acetylsalicylic%22", None, headers)
-    conn.request("GET", "/drug/label.json?limit=20&search=substance_name:%22ASPIRIN%22", None, headers)
+    conn.request("GET", "/drug/label.json?limit=100&skip="+str(skip_number)+"&search=substance_name:%22ASPIRIN%22", None, headers)
 
     r1 = conn.getresponse()
     print(r1.status, r1.reason)
     estructura_json = r1.read().decode("utf-8")
     conn.close()
 
-
     label = json.loads(estructura_json)
     for i in range (len (label['results'])):
-        medicamento_info=label['results'][i]
-        print ('ID: ',medicamento_info['id'])
-        if (medicamento_info['openfda']):
-            print('Fabricante: ', medicamento_info['openfda']['manufacturer_name'][0])
+        info_medicamento=label['results'][i]
+        print ('ID: ',info_medicamento['id'])
+        if (info_medicamento['openfda']):
+            print('Fabricante: ', info_medicamento['openfda']['manufacturer_name'][0])
     if (len(label['results'])<100):
          break
     skip_number=skip_number+100
