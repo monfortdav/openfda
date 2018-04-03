d55f302fac1180bbc1801ef4eb892182332a2eb5
parallels <david_monfort_martin@hotmail.com>
2018-04-03 12:08:16 +0200

    Creamos el fichero programa1

--- openfda-1/programa1.py
+++ openfda-1/programa1.py
@@ -1,27 +1,23 @@
 import http.client
 import json
+#Módulos o librerías para descargarnos info en páginas web de forma más fácil
 
 headers = {'User-Agent': 'http-client'}
+#Cabeceras, es un diccionario con clave-valor
 
 conn = http.client.HTTPSConnection("api.fda.gov")
 conn.request("GET", "/drug/label.json", None, headers)
 r1 = conn.getresponse()
-print(r1.status, r1.reason)
+print(r1.status, r1.reason) #Depuración
 estructura_json = r1.read().decode("utf-8")
 conn.close()
 
-#escribimos a fichero lo recibido
-#fichero = open ('label.json', 'w')
-#fichero.write(label_raw)
-#fichero.close()
-#fin escribir
-
 
 label = json.loads(estructura_json)
-medicamento_info=label['results'][0]
+info_medicamento=label['results'][0]
 
-print ('ID: ',medicamento_info['id'])
-print ('Proposito: ',medicamento_info['purpose'][0])
+#Obtenemos el id, el propósito y el fabricante del medicamento
+print ('ID: ',info_medicamento['id'])
+print ('Proposito: ',info_medicamento['purpose'][0])
 
-print ('Fabricante: ',medicamento_info['openfda']['manufacturer_name'][0])
-
+print ('Fabricante: ',info_medicamento['openfda']['manufacturer_name'][0])
